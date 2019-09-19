d9ee95c2dc298a9cd87edb5208b7b9594e2036c4
remorses <beats.by.morse@gmail.com>
2019-09-19 13:31:59 +0200

    entrypoint: input not github prefix

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ echo starting
 
 ([ -z "$GITHUB_ONLY_ON_COMMIT" ] || [ "$GITHUB_ONLY_ON_COMMIT" = "$GITHUB_COMMIT_MESSAGE" ]) || exit 0
 
-[ -z "$GITHUB_VERSION_FILE" ] && GITHUB_VERSION_FILE=./VERSION
+[ -z "$INPUT_VERSION_FILE" ] && INPUT_VERSION_FILE=./VERSION
 # [ \( -z "$REPOSITORY" \) -a \( ! -z "$CIRCLE_PROJECT_REPONAME" \) ] && REPOSITORY="$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
-# [ -f $GITHUB_VERSION_FILE ] || (echo "file containing current version is needed, default is ./VERSION, customizable with GITHUB_VERSION_FILE env var" && exit 1)
+# [ -f $INPUT_VERSION_FILE ] || (echo "file containing current version is needed, default is ./VERSION, customizable with INPUT_VERSION_FILE env var" && exit 1)
 # [ -z "$REPOSITORY" ] && (echo "env var REPOSITORY is needed when executing outside of circleci, (for exampe REPOSITORY=user/repo)" && exit 1)
 
 git config user.email "bump@version.com"
@@ -28,15 +28,15 @@ git pull --commit --no-edit https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITO
 # [ -n "$CONTINUE" ] || exit 0
 
 
-(/script/bump --filename $GITHUB_VERSION_FILE)
-export VERSION=`cat $GITHUB_VERSION_FILE`
+(/script/bump --filename $INPUT_VERSION_FILE)
+export VERSION=`cat $INPUT_VERSION_FILE`
 
-[ -n "$GITHUB_PREFIX" ] || (echo 'running _sed'; /_sed)
-[ -z "$GITHUB_PREFIX" ] || (echo 'running _sed_with_prefix'; /_sed_with_prefix)
+[ -n "$INPUT_PREFIX" ] || (echo 'running _sed'; /_sed)
+[ -z "$INPUT_PREFIX" ] || (echo 'running _sed_with_prefix'; /_sed_with_prefix)
 
 git add -A 
-git commit -m "${GITHUB_PREFIX} ${VERSION}"  -m "[skip ci]"
-[ -n "$GITHUB_PREFIX" ] && (git tag -a "${GITHUB_PREFIX}_${VERSION}" -m "[skip ci]") || (git tag -a "${VERSION}" -m "[skip ci]")
+git commit -m "${INPUT_PREFIX} ${VERSION}"  -m "[skip ci]"
+[ -n "$INPUT_PREFIX" ] && (git tag -a "${INPUT_PREFIX}_${VERSION}" -m "[skip ci]") || (git tag -a "${VERSION}" -m "[skip ci]")
 
 git show-ref
 git push --follow-tags "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" HEAD:master
