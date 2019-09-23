70b30d97bacd22ff89bf536ca82445b5e9f2ab99
remorses <beats.by.morse@gmail.com>
2019-09-23 20:51:22 +0200

    made it

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import * as core from '@actions/core'
+import * as fs from 'fs'
 import * as github from '@actions/github'
-import createTag from './createTag'
+import {createTag, } from './createTag'
+import {capitalize, bump, replacePattern} from './support'
+
+const versionRegex = /[0-9]+\.[0-9]+\.[0-9]+/
 
 async function run() {
     const versionPath = core.getInput('version_file',)
     const prefix = core.getInput('prefix',)
+    const version = fs.readFileSync(versionPath, 'utf8').toString().trim()
+    const newVersion = bump(version)
+    if (prefix) {
+        const pattern = new RegExp('\\[bump if ' + prefix + '\\]')
+        await replacePattern(pattern, versionRegex, newVersion)
+    } else {
+        await replacePattern(/.*\[bump\].*/, versionRegex, newVersion)
+    }
     await createTag({
-        tagName: 'xxx',
+        tagName: prefix ? prefix + newVersion : newVersion,
+        tagMsg: `${capitalize(prefix)} Version ${newVersion}`
     })
-    
 }
 
 run()
---
+++ src/support.ts
@@ -0,0 +1,51 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import globby from 'globby'
+
+export const replacePattern = async (
+    pattern: RegExp,
+    replacer: RegExp,
+    value: string
+) => {
+    const files = await globby('**', {
+        gitignore: true,
+        expandDirectories: true
+    })
+    files.forEach((name) => {
+        let found = false
+        const lines = fs
+            .readFileSync(name, 'utf8')
+            .toString()
+            .split('\n')
+        const result = lines
+            .reduce(
+                (acc, last) => {
+                    if (acc[acc.length - 1].search(pattern) !== -1) {
+                        found = true
+                        last = last.replace(replacer, value)
+                    }
+                    return [...acc, last]
+                },
+                ['']
+            )
+            .slice(1)
+            .join('\n')
+        if (found) {
+            fs.writeFileSync(name, result, 'utf8')
+        }
+    })
+    //
+}
+
+export function bump(version: string): string {
+    return version.replace(/^([\d\.]+)([\-|\.])(\d+)$/, function() {
+        return arguments[1] + arguments[2] + (Number(arguments[3]) + 1)
+    })
+}
+
+export const capitalize = prefix => {
+    return prefix.charAt(0).toUpperCase() + prefix.slice(1)
+}
+// console.log(bump('0.1.0'))
+
+// replacePattern(/.*\[bump\].*/, /[0-9]+\.[0-9]+\.[0-9]+/, '0.1.2')
