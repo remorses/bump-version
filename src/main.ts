100179d829165ab1d19c4833e4cc793005acd470
remorses <beats.by.morse@gmail.com>
2019-09-23 21:40:21 +0200

    write new version in version_file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
 import * as github from '@actions/github'
-import {createTag, } from './createTag'
-import {capitalize, bump, replacePattern} from './support'
+import { createTag } from './createTag'
+import { capitalize, bump, replacePattern } from './support'
 import commit from './commit'
-
 
 const versionRegex = /[0-9]+\.[0-9]+\.[0-9]+/
 
 async function run() {
-    const versionPath = core.getInput('version_file',)
-    const prefix = core.getInput('prefix',).trim()
-    const version = fs.readFileSync(versionPath, 'utf8').toString().trim()
+    const versionPath = core.getInput('version_file')
+    const prefix = core.getInput('prefix').trim()
+    const version = fs
+        .readFileSync(versionPath, 'utf8')
+        .toString()
+        .trim()
     const newVersion = bump(version)
+    fs.writeFileSync(versionPath, newVersion, 'utf8')
     if (prefix) {
         const pattern = new RegExp('\\[bump if ' + prefix + '\\]')
         await replacePattern(pattern, versionRegex, newVersion)
@@ -20,17 +23,18 @@ async function run() {
         await replacePattern(/.*\[bump\].*/, versionRegex, newVersion)
     }
     const tagMsg = `${capitalize(prefix) + ' '}Version ${newVersion}`
-    await commit({
-        USER_EMAIL: 'bump@version.com',
-        USER_NAME: 'bump-version',
-        GITHUB_TOKEN: process.env.GITHUB_TOKEN as string,
-        MESSAGE: tagMsg,
-    })
-    await createTag({
-        tagName: prefix ? prefix + '_' + newVersion : newVersion,
-        tagMsg,
-    })
-
+    await Promise.all([
+        commit({
+            USER_EMAIL: 'bump@version.com',
+            USER_NAME: 'bump-version',
+            GITHUB_TOKEN: process.env.GITHUB_TOKEN as string,
+            MESSAGE: tagMsg
+        }),
+        createTag({
+            tagName: prefix ? prefix + '_' + newVersion : newVersion,
+            tagMsg
+        })
+    ])
 }
 
 run()
