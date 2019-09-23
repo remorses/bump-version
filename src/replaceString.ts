2120d8171ec5b7b9f0aa5a2f6a9ae7f9da4e3d07
remorses <beats.by.morse@gmail.com>
2019-09-23 19:31:10 +0200

    working

---
+++ src/replaceString.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import glob from 'glob'
+import globby from 'globby'
+
+
+
+
+const read = async (pattern: RegExp, replacer: RegExp, value: string) => {
+    const files = await globby('**', {gitignore: true, expandDirectories: true})
+    files.map(name => {
+        const lines = fs.readFileSync(name, 'utf8').toString().split('\n')
+        const result = lines.reduce((acc, last) => {
+            // if (acc[acc.length - 1].search(pattern)) {
+            //     console.log('replacing')
+            //     last = last.replace(replacer, value)
+            // }
+            return [...acc, last]
+        }, ['']).join('\n')
+        fs.writeFileSync(name, 'utf8', result)
+    })
+    // 
+}
+
+read(/.*\[bump\].*/, /(.*)([0-9]+\.[0-9]+\.[0-9]+)(.*)/, '0.1.2')
+
+export default read
